f7055f9d60fe57bada0c16d16fb92f41e1494d0e
SuperLakshya <72199086+SuperLakshya@users.noreply.github.com>
2025-04-08 11:27:00 +0530

    Delete token.js

--- token.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const token = process.env.HF_API_TOKEN;
-  if (!token) {
-    return res.status(500).json({ error: 'Token not found' });
-  }
-  res.status(200).json({ token });
-}
